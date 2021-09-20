4f6964c5499ffc0eafaa42f11a86c920401f2898
Ma77i <mattiasuru@gmail.com>
2021-09-20 15:04:39 -0300

    probando pushear cambios en la rama porque no me deja en el master

---
+++ JS/main.js
@@ -0,0 +1,30 @@
+// Función para mostrar contacto
+function mostrarContacto() {
+	$('.overlay').fadeIn('slow')
+	$('.modalContacto').animate({
+		left: '0',
+		right: '0'}
+	)
+}
+// Función para ocultar modal
+function ocultarModal() {
+	$('.modalContacto').animate({
+		left: '-200vw'}
+	)
+	$('.overlay').fadeOut('slow')
+}
+
+// Función para mostrar y ocultar boton de whatsapp
+$(document).ready(function () {
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 200) {
+      $("#whatsapp-btn").fadeIn(300);
+		} else {
+      $("#whatsapp-btn").fadeOut(300);
+    }
+	});
+
+	$('#contacto').click(mostrarContacto)
+	$('.overlay').click(ocultarModal)
+});
+
