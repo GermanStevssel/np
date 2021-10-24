85b1624b507ff64da973b0d343cb7862e902a6aa
GermanStevssel <germancstevssel@gmail.com>
2021-10-24 16:19:45 -0300

    comenzando desktop

--- js/main.js
+++ js/main.js
@@ -1,48 +1,47 @@
 // Función para mostrar modal de contacto
 function mostrarContacto() {
-	$('.overlay').fadeIn('slow')
-	$('#contenedorContacto').addClass('visible');
+	$(".overlay").fadeIn("slow");
+	$("#contenedorContacto").addClass("visible");
 }
 // Función para ocultar modal de contacto
 function ocultarContacto() {
-	$('.overlay').fadeOut('slow')
-  $('#contenedorContacto').removeClass('visible')
+	$(".overlay").fadeOut("slow");
+	$("#contenedorContacto").removeClass("visible");
 }
 // Función ocultar modal contacto con tecla ESC
 function ocultarContactoEsc(e) {
-  if (e.key == "Escape") {
-    $(".modalContacto").removeClass('visible');
-    $('.overlay').fadeOut('slow')
-  }
+	if (e.key == "Escape") {
+		$(".modalContacto").removeClass("visible");
+		$(".overlay").fadeOut("slow");
+	}
 }
 
 $(document).ready(function () {
-  // Función para mostrar y ocultar boton de whatsapp
+	// Función para mostrar y ocultar boton de whatsapp
 	$(window).scroll(function () {
 		if ($(this).scrollTop() > 30) {
-      $("#whatsapp-btn").fadeIn(300);
+			$("#whatsapp-btn").fadeIn(300);
 		} else {
-      $("#whatsapp-btn").fadeOut(300);
-    }
+			$("#whatsapp-btn").fadeOut(300);
+		}
 	});
 
-  // Mostrar 2do logo
-  $('.navbar-toggler').click(() => {
-    if($('.contLogo>a').hasClass('visible')) {
-      $('.contLogo>a').removeClass('visible')
-      $('.logoZanahoria').removeClass('visible')
-      $('.logo__index').toggle(600)
-    } else {
-      $('.contLogo>a').addClass('visible')
-      $('.logoZanahoria').addClass('visible')
-      $('.logo__index').toggle(600)
-    } 
-  })
+	// Mostrar 2do logo
+	$(".navbar-toggler").click(() => {
+		if ($(".contLogo>a").hasClass("visible")) {
+			$(".contLogo>a").removeClass("visible");
+			$(".logo-zanahoria").removeClass("visible");
+			$(".logo__index").toggle(600);
+		} else {
+			$(".contLogo>a").addClass("visible");
+			$(".logo-zanahoria").addClass("visible");
+			$(".logo__index").toggle(600);
+		}
+	});
 
-	let contacto = $('[data-open]')[0]
+	let contacto = $("[data-open]")[0];
 
-	$(contacto).click(mostrarContacto)
-	$('.overlay').click(ocultarContacto)
-  $(document).keyup(ocultarContactoEsc);
+	$(contacto).click(mostrarContacto);
+	$(".overlay").click(ocultarContacto);
+	$(document).keyup(ocultarContactoEsc);
 });
-
